b37e84ae2647806c3d0599e6fed5faaa91c14f02
juliolimoli <juliolimolisilva@gmail.com>
2023-09-11 22:33:19 +0200

    fixing the client variable

--- src/app.js
+++ src/app.js
@@ -4,15 +4,6 @@ const {
     StopInstancesCommand 
     } = require('@aws-sdk/client-ec2');
 
-async function stopInstance() {
-    try {
-        const data = await ec2Client.send(stopCommand);
-        console.log("Instance stopped successfully", data);
-    } catch (error) {
-        console.error("Error stopping instance", error);
-    }
-}
-
 exports.handler = async (event) => {
     const client = new EC2Client({ region: "sa-east-1" })
     const instanceId = event.instanceId; // The EC2 instance ID
@@ -22,10 +13,10 @@ exports.handler = async (event) => {
 
     try {
         if (action === 'start') {
-            const data = await ec2Client.send(startCommand);
+            const data = await client.send(startCommand);
             console.log("Instance started successfully", data);
         } else if (action === 'stop') {
-            const data = await ec2Client.send(stopCommand);
+            const data = await client.send(stopCommand);
             console.log("Instance stopped successfully", data);
         } else {
             return 'Invalid action. Use "start" or "stop".';
